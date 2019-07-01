cb921a00dccd0171d41e5c8001a6b1fb77c735ff
AlexDarchuk <51719428+AlexDarchuk@users.noreply.github.com>
2019-07-01 19:18:50 +0300

    1
    1

---
+++ css4.js
@@ -0,0 +1,84 @@
+//console.log('Hello World');
+
+//for (let i = 100; i >= 0; i -=2) {
+//  if (i % 5 === 0)
+//  continue;
+ // if ( i % 2 === 0 && i % 5 !== 0)
+//console.log(i);
+//}
+
+//let i = 0;
+//while (i < 10) {
+  //console.log(i);
+//}
+
+//let counter = 0;
+//while (Math.random() < 0.9) {
+  //counter++;
+  //console.log(counter);
+//}
+
+//let counter = 0;
+//do {
+  //counter++;
+  //console.log(counter);
+//}
+
+// function padIt(str, n) {
+//   while (n > 0) {
+//     if ( n % 2 === 0) {
+//       str = '*' + str;
+//     } else {
+//       str = str + '*';
+//     }
+//     n--;
+//   }
+//   return str;
+// }
+
+// padIt('hello', 50);
+
+// let products = [];
+// products[0] = 'Tesla X';
+// products[1] = 'Tesla S';
+// products[2] = 'ZAZ Sens';
+// products.push('BMW X7');
+// products.push('Lada 2109');
+// console.log(products.length);
+// console.log(products[products.length-1]);
+
+// products[10] = 'Toyota Corolla';
+
+// for (let i = 0; i < products.length; i++) {
+  // console.log(i+1, products[i]);
+// }
+
+ let products = [Tesla X, Tesla S, ZAZ Sens, BMW X7, Lada 2109];
+  console.log(products.length);
+  console.log(products[products.length-1]);
+     products.forEach( el => console.log(el) );
+  // let prices = [100000, 200000, 10000, 150000, 2000, 20000];
+//  prices.forEach( el => console.log(el) );
+//  for (let.products i = 0; i < products.length; i++) {
+  //  console.log(products[i], prices[i]);
+//  }
+
+// let products = [];
+// products[0] = 'Tesla X';
+// products[1] = 'Tesla S';
+// products[2] = 'ZAZ Sens';
+// products.push('BMW X7');
+// products.push('Lada 2109');
+// console.log(products.length);
+// console.log(products[products.length-1]);
+
+// products[5] = 'Toyota Corolla';
+
+// products.forEach( el => console.log(el) );
+
+// let prices = [100000, 200000, 10000, 150000, 2000, 20000];
+// prices.forEach( el => console.log(el) );
+
+// for (let i = 0; i < products.length; i++) {
+  // console.log(products[i], prices[i]);
+// }
